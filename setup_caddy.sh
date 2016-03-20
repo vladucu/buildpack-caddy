dba1d51fc64e261599524256b9afa7135b46f1fd
Frank Hamand <frankhamand@gmail.com>
2016-03-20 16:52:18 +0000

    Update build

--- setup_caddy.sh
+++ setup_caddy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed s/\$PORT/$PORT/g Caddyfile > Caddyfile
-echo "import ./Caddyfile" > HerokuCaddyfile
+sed s/\$PORT/$PORT/g Caddyfile > Caddyfile.2
+echo "import ./Caddyfile.2" > HerokuCaddyfile
